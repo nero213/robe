c39b1c4c2853ff525095dc51d8c3f32ae7e124a6
Robe Kyle P. Salangad <147113315+nero213@users.noreply.github.com>
2025-04-22 09:18:51 +0800

    Update TypeWrite.js

--- TypeWrite.js
+++ TypeWrite.js
@@ -1,38 +1,53 @@
-  // Typewriter effect function
-  function typeWriter(elementId, text, speed, pause) {
-    let i = 0;
-    let element = document.getElementById(elementId);
-    element.innerHTML = "";  // Clear the content initially
-
-    // Function to type one character at a time
-    function type() {
-      if (i < text.length) {
-        element.innerHTML += text.charAt(i);
-        i++;
-        setTimeout(type, speed);
-      } else {
-        // After typing is done, wait for 'pause' time before erasing
-        setTimeout(erase, pause);
-      }
+// Typewriter effect function
+function typeWriter(elementId, text, speed, pause) {
+  const phrases = [
+    "Welcome to My Portfolio",
+    "I'm Robe Kyle P. Salangad",
+    "I Create Web Experiences",
+    "Let's Build Something Amazing"
+  ];
+  
+  let phraseIndex = 0;
+  let charIndex = 0;
+  let isDeleting = false;
+  let element = document.getElementById(elementId);
+  
+  function type() {
+    // Current phrase
+    const currentPhrase = phrases[phraseIndex];
+    
+    // If deleting, remove a character
+    if (isDeleting) {
+      element.innerHTML = currentPhrase.substring(0, charIndex - 1);
+      charIndex--;
+    } else {
+      // If typing, add a character
+      element.innerHTML = currentPhrase.substring(0, charIndex + 1);
+      charIndex++;
     }
-
-    // Function to erase the text one character at a time
-    function erase() {
-      if (i > 0) {
-        element.innerHTML = text.substring(0, i - 1);
-        i--;
-        setTimeout(erase, speed);
-      } else {
-        // Once erasing is done, start typing again
-        setTimeout(type, pause);
-      }
+    
+    // Type speed - faster when deleting
+    let typeSpeed = isDeleting ? speed / 2 : speed;
+    
+    // If completed typing the current phrase
+    if (!isDeleting && charIndex === currentPhrase.length) {
+      // Pause at end of typing
+      typeSpeed = pause;
+      isDeleting = true;
+    } else if (isDeleting && charIndex === 0) {
+      // Move to next phrase after complete deletion
+      isDeleting = false;
+      phraseIndex = (phraseIndex + 1) % phrases.length;
     }
-
-    // Start the typing effect
-    type();
+    
+    setTimeout(type, typeSpeed);
   }
+  
+  // Start typing
+  type();
+}
 
-  // Call the typewriter effect when the page loads
-  window.onload = function() {
-    typeWriter("typewriter", "Welcome to Website My Name is Robe Kyle P. Salangad", 50, 1000); // Speed 150ms, pause 1000ms before erasing
-  };
+// Call the typewriter effect when the page loads
+window.onload = function() {
+  typeWriter("typewriter", "", 100, 2000); // Speed 100ms, pause 2000ms
+};
